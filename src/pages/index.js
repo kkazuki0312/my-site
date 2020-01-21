94265b74f2757960a9cbaec2f7fe5afd617d8bf6
kkazuki0312 <kkazuki0312@yahoo.co.jp>
2020-01-21 23:41:03 +0900

    スタイル変更

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,17 +26,19 @@ const IndexPage = () => (
           <tr>
             <td className="center">
               <p className="pink forestgreen">あなたは <img src="http://www.rays-counter.com/d451_f6_022/5e2413911e8f4/" alt="アクセスカウンター" border="0" /> 人目の訪問者です！！！</p>
-              <span>ゆっくりしていってね</span>
-            </td>
+              <div className="return">
+        <p>ゆっくりしていってね</p>
+      </div>            </td>
           </tr>
         </tbody>
       </table>
     </div>
     <h1 className="darkmagenta">【きょうのメッセージ】</h1>
-    <h2 className="red">本気になれば自分が変わる！本気になれば全てが変わる！！！！</h2>
+    <h2 className="red">もっと熱くなれよ！熱い血燃やしてけよ！！人間熱くなったときがホントの自分に出会えるんだ！！！！！！！！</h2>
     <div className="marquee">
-        <p>★★★★★相互リンク募集中★★★★★</p>
+      <p>★★★★★相互リンク募集中★★★★★</p>
     </div>
+    <h3 className="brown">☆★☆松戸市のお天気情報☆★☆</h3>
   </div>
 )
 
