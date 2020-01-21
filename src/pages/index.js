d61c0f7cba7c6ef5e0d67c393016cf1919ea6859
kkazuki0312 <kkazuki0312@yahoo.co.jp>
2020-01-22 00:59:42 +0900

    一旦コミット

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+
 const IndexPage = () => (
   // <Layout>
   //   <SEO title="Home" />
@@ -27,18 +28,18 @@ const IndexPage = () => (
             <td className="center">
               <p className="pink forestgreen">あなたは <img src="http://www.rays-counter.com/d451_f6_022/5e2413911e8f4/" alt="アクセスカウンター" border="0" /> 人目の訪問者です！！！</p>
               <div className="return">
-        <p>ゆっくりしていってね</p>
-      </div>            </td>
+                <p>ゆっくりしていってね</p>
+              </div>
+            </td>
           </tr>
         </tbody>
       </table>
     </div>
     <h1 className="darkmagenta">【きょうのメッセージ】</h1>
-    <h2 className="red">もっと熱くなれよ！熱い血燃やしてけよ！！人間熱くなったときがホントの自分に出会えるんだ！！！！！！！！</h2>
+    <h2 className="red">もっと熱くなれよ！熱い血燃やしてけよ！！<br/>人間熱くなったときがホントの自分に出会えるんだ！！！！！！！！</h2>
     <div className="marquee">
       <p>★★★★★相互リンク募集中★★★★★</p>
     </div>
-    <h3 className="brown">★☆★☆松戸市のお天気情報☆★☆★</h3>
   </div>
 )
 
