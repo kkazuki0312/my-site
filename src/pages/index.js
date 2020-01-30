0cca98b985132a656f7aed0a6e966ccca24e4fc2
Gen Umegaki <g_umegaki@waku-2.com>
2020-01-30 10:45:36 +0900

    format

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,10 +21,10 @@ const IndexPage = () => {
   }, [])
 
   function logoClick(e) {
-    const div = document.getElementById("gorilla");
-    const img = document.getElementById("logo");
-    const clone = img.cloneNode(true);
-		div.appendChild(clone);
+    const div = document.getElementById("gorilla")
+    const img = document.getElementById("logo")
+    const clone = img.cloneNode(true)
+    div.appendChild(clone)
   }
 
   // <Layout>
@@ -40,64 +40,70 @@ const IndexPage = () => {
   return (
     <div className="wrap">
       <header className="leftNavigation">
-        <div　className="profile">
+        <div className="profile">
           <Link to="/profile/">プロフィール</Link>
         </div>
-        <div　className="bss">
+        <div className="bss">
           <Link to="/bss/">掲示板</Link>
         </div>
-        <div　className="compatible">
+        <div className="compatible">
           <Link to="/compatible/">僕との相性チェック</Link>
         </div>
-        <div　className="quiz">
+        <div className="quiz">
           <Link to="/quiz/">クイズ</Link>
         </div>
-        <div　className="link">
-        <Link to="/link/">リンク</Link>
+        <div className="link">
+          <Link to="/link/">リンク</Link>
         </div>
       </header>
-    <div className="content">
-      <Helmet>
-        <title>{title}</title>
-        <link rel="icon" href={icon}></link>
-      </Helmet>
-      <div>
-        <div className="marquee">
-          <p>☆☆☆☆☆柏原のサイトへようこそ☆☆☆☆☆</p>
+      <div className="content">
+        <Helmet>
+          <title>{title}</title>
+          <link rel="icon" href={icon}></link>
+        </Helmet>
+        <div>
+          <div className="marquee">
+            <p>☆☆☆☆☆柏原のサイトへようこそ☆☆☆☆☆</p>
+          </div>
+          <table>
+            <tbody>
+              <tr>
+                <td className="center">
+                  <p className="pink forestgreen">
+                    あなたは{" "}
+                    <img
+                      className="center"
+                      src="http://www.rays-counter.com/d451_f6_022/5e2413911e8f4/"
+                      alt="アクセスカウンター"
+                    />{" "}
+                    人目の訪問者です！！！
+                  </p>
+                  <div className="return">
+                    <p>ゆっくりしていってね</p>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-        <table>
-          <tbody>
-            <tr>
-              <td className="center">
-                <p className="pink forestgreen">
-                  あなたは{" "}
-                  <img className="center"
-                    src="http://www.rays-counter.com/d451_f6_022/5e2413911e8f4/"
-                    alt="アクセスカウンター"
-                  />{" "}
-                  人目の訪問者です！！！
-                </p>
-                <div className="return">
-                  <p>ゆっくりしていってね</p>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <h1 className="darkmagenta">【きょうのメッセージ】</h1>
+        <h2 className="red">
+          生まれた時から目の見えない人に
+          <br />
+          空の青さを伝える時何て言えばいいんだ？
+          <br />
+          こんな簡単なことさえ言葉にできない俺は芸人失格だよ
+          <br />
+          by 江頭2：50
+        </h2>
+        <div className="marquee">
+          <p>★★★★★相互リンク募集中★★★★★</p>
+        </div>
+        <h3>↓↓↓↓↓ゴリラをクリックしてみてね↓↓↓↓↓</h3>
+        <div id="gorilla" className="gorilla gorilla1">
+          <img id="logo" src={logo} alt="Logo" onClick={logoClick} />
+        </div>
       </div>
-      <h1 className="darkmagenta">【きょうのメッセージ】</h1>
-      <h2 className="red">生まれた時から目の見えない人に<br/>
-        空の青さを伝える時何て言えばいいんだ？<br/>
-        こんな簡単なことさえ言葉にできない俺は芸人失格だよ<br/>
-        by 江頭2：50</h2>
-      <div className="marquee">
-        <p>★★★★★相互リンク募集中★★★★★</p>
-      </div>
-      <h3>↓↓↓↓↓ゴリラをクリックしてみてね↓↓↓↓↓</h3>
-      <div id="gorilla" className="gorilla gorilla1">
-        <img id="logo" src={logo} alt="Logo" onClick={logoClick} />
-      </div>
-    </div>
     </div>
   )
 }
